e02bb7b174a0cc1c59a6074f8002f98b8b9d226a
joshuadias06 <joshuadiasbf@gmail.com>
2025-03-15 00:58:02 -0300

    Updated and Created layout agender screen

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,5 @@
 describe('Primeiro Teste', () => {
   it('passes', () => {
-    cy.visit('http://127.0.0.1:5500/layouts/login.html')
-    cy.wait(2000);
-    cy.get('#email').type('joshua@gmail.com');
-    cy.get('#senha').type('Admin@123')
-    cy.get('#submit-btn').click()
+   
   })
 })
